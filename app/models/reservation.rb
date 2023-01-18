3a23f57915700314d5c682476ee69ad64ba529db
AJ Villalobos <hello@ajvillalobos.com>
2023-01-18 16:50:56 +0800

    Add method #airbnb_details only accessible if source is airbnb

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,5 +1,16 @@
 class Reservation < ApplicationRecord
+  class MethodNotAllowed < StandardError
+  end
+
   belongs_to :guest
 
   validates :code, uniqueness: true
+
+  define_method :airbnb_details do
+    if source == 'airbnb'
+      additional_details
+    else
+      raise MethodNotAllowed
+    end
+  end
 end
